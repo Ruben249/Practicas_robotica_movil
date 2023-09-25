b4384a6b4c28e00c87cf231ba30242aaf1a6719e
rbautista2021 <102288264+Ruben249@users.noreply.github.com>
2023-09-25 15:13:03 +0200

    Add files via upload

--- vacuum_cleaner_newmanager.py
+++ vacuum_cleaner_newmanager.py
@@ -1,65 +1,96 @@
 from GUI import GUI
 from HAL import HAL
 import time
-# Enter sequential code!
-  
-tiempo = time.time()
-print(tiempo)
-  
+import random
+
+actual_time = time.time()
+
+
 def forward_state(n):
-  HAL.setV(n)
-  print("El robot avanza.")
-  
+    HAL.setW(0)
+    HAL.setV(n)
+
+
 def backward_state(n):
-  HAL.setV(-n)
-  print("El robot retrocede.")    
-    
+    HAL.setW(0)
+    HAL.setV(-n)
+    print("El robot retrocede.")
+
+
 def spin_state(n):
-  HAL.setW(n)
-  print("El robot gira.")
-  
+    HAL.setW(n)
+    HAL.setV(1)
+    print("El robot gira.")
+
+
 def spiral_state(n, m):
-  HAL.setV(n)
-  HAL.setW(m)
-  print("El robot esta haciendo una espiral.")
-    
-forward_state(2)
+    HAL.setV(n)
+    HAL.setW(m)
+    print("El robot esta haciendo una espiral.")
+
+
+def contador_choque():
+    tiempo_choque = time.time()
+    backward_state(0.5)
+    controlador = True
+
+    while controlador:
+        tiempo_actualizado = time.time()
+        if tiempo_actualizado - tiempo_choque >= 2:
+            controlador = False
+        else:
+            backward_state(0.5)
+
+
+def contador_giro(n):
+    tiempo_giro = time.time()
+    spin_state(n)
+    controlador = True
+
+    while controlador:
+        tiempo_actualizado = time.time()
+        if tiempo_actualizado - tiempo_giro >= 1:
+            forward_state(1)
+            controlador = False
+        else:
+            spin_state(n)
+
+
+forward_state(1)
+
 while True:
-    spiral_state(0.4, 0.5)
+    new_time = time.time()
+
+    if new_time - actual_time >= 7:
+        actual_time = new_time
+        dado = random.randint(1, 100)
+        print("Lanzar dados")
+        if dado <= 33:
+            vel_angular = random.uniform(0, 1.5)
+            vel_lineal = vel_angular + 0.4
+            spiral_state(vel_lineal, vel_angular)
+
     if HAL.getBumperData().state == 1:
         if HAL.getBumperData().bumper == 0:
-          backward_state(2)
-          spin_state(1)
-          print("Choque a la izquierda")
+            contador_choque()
+            contador_giro(2.5)
+            print("Choque a la izquierda")
+
+        elif HAL.getBumperData().bumper == 2:
+            print("Choque derecha")
+            contador_choque()
+            contador_giro(-1.3)
 
         else:
-          print("Choque centro-izquierda")
-          backward_state(2)
-          spin_state(-1)
-      
+            print("Choque centro")
+            contador_choque()
+            lado = random.randint(1, 2)
+            if lado == 1:
+                contador_giro(-1.2)
+            else:
+                contador_giro(1.4)
+
+
     else:
-      print("no choca")
-    #Comprobar laser  
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    # Enter iterative code!
+        print("no choca")
+    # Comprobar laser  
